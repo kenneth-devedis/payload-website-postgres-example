85ecc4dc0a9d6c7a52f996a1ea12abc52b96ff66
Kenneth Radunz <kenneth.radunz@devedis.ch>
2025-02-27 17:44:13 +0100

    stuff

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
-import * as migration_20250224_151910 from './20250224_151910';
+import * as migration_20250227_161027 from './20250227_161027';
+import * as migration_20250227_162013 from './20250227_162013';
 
 export const migrations = [
   {
-    up: migration_20250224_151910.up,
-    down: migration_20250224_151910.down,
-    name: '20250224_151910'
+    up: migration_20250227_161027.up,
+    down: migration_20250227_161027.down,
+    name: '20250227_161027',
+  },
+  {
+    up: migration_20250227_162013.up,
+    down: migration_20250227_162013.down,
+    name: '20250227_162013'
   },
 ];
