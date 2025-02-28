89a5a269945a697820bba58185103b068679c140
Kenneth Radunz <kenneth.radunz@devedis.ch>
2025-02-28 15:50:38 +0100

    mup

--- src/app/(frontend)/public-api/testimonials/route.ts
+++ src/app/(frontend)/public-api/testimonials/route.ts
@@ -1,39 +1,9 @@
 import { CollectionSlug, extractJWT, getPayload } from 'payload'
 import configPromise from '@payload-config'
 import { NextRequest, NextResponse } from 'next/server'
-import { jwtVerify } from 'jose'
 
-type JWTToken = {
-  collection: string
-  id: string
-}
-
-export async function GET(req: NextRequest) {
+export async function GET({ headers }: NextRequest) {
   const payload = await getPayload({ config: configPromise })
-  const token = extractJWT({ headers: req.headers, payload })
-  console.log(token)
-  const secretKey = new TextEncoder().encode(payload.secret)
-  const { payload: decodedPayload } = await jwtVerify<JWTToken>(token, secretKey)
-  console.log(decodedPayload)
-  const user = await payload.findByID({
-    id: decodedPayload.id,
-    collection: decodedPayload.collection as CollectionSlug,
-    depth: 0
-  })
-  console.log(user)
-
-  // if (user && (!collection.config.auth.verify || user._verified)) {
-  //   user.collection = collection.config.slug
-  //   user._strategy = strategyName
-  //   return {
-  //     user: user as User,
-  //   }
-  // } else {
-  //   if (headers.get('DisableAutologin') !== 'true') {
-  //     return await autoLogin({ isGraphQL, payload, strategyName })
-  //   }
-  //   return { user: null }
-  // }
-  // const result = await payload.auth({ headers: req.headers})
+  const { user } = payload.auth({ headers })
   return NextResponse.json(user, { status: 200 })
 }
