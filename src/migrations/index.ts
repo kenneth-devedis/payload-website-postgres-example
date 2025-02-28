c265dbaca66ff3ec9a87025e2bb991ef524237af
Kenneth Radunz <kenneth.radunz@devedis.ch>
2025-02-28 08:59:05 +0100

    generate migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250228_075726 from './20250228_075726';
+
+export const migrations = [
+  {
+    up: migration_20250228_075726.up,
+    down: migration_20250228_075726.down,
+    name: '20250228_075726'
+  },
+];
